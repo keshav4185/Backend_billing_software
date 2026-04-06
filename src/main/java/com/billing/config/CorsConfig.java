62ce2359cd60fd02d03862aacc1274dddaf15211
Sadanand Belote <sadanandbelote1104@gmail.com>
2026-04-06 14:56:19 +0530

    Chnages in cors Origin

--- src/main/java/com/billing/config/CorsConfig.java
+++ src/main/java/com/billing/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://footware-billing.vercel.app")
+                .allowedOriginPatterns("https://footware-billing.vercel.app", "http://localhost:*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
