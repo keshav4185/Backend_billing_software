99b5d370c12fc154bbc077b15f3b8b18dddf8293
Kaushal Mali <kaushalmali@Kaushals-Laptop-3.local>
2026-04-24 12:41:22 +0530

    Trigger schema update

--- src/main/java/com/billing/repository/AdminRepository.java
+++ src/main/java/com/billing/repository/AdminRepository.java
@@ -9,6 +9,6 @@ import com.billing.entity.Admin;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-
+    // Added comment to trigger redeploy and schema update
     Optional<Admin> findByUsername(String username);
 }
