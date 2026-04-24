104f8440e5627efcd72a45536e991fa21077d28d
Kaushal Mali <kaushalmali@Kaushals-Laptop-3.local>
2026-04-24 12:03:23 +0530

    corsconfig

--- src/main/java/com/billing/config/CorsConfig.java
+++ src/main/java/com/billing/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://footware-billing.vercel.app", "http://http://localhost:5174/")
+                        .allowedOrigins("https://footware-billing.vercel.app", "http://localhost:5174")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
